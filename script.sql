2070bd68dbef4a41157645db3e9b7359cf15dbaa
brunoreisc <bruno.reisc@hotmail.com>
2018-11-15 13:06:53 -0200

    Add files via upload
    Adding script for database creation.

---
+++ script.sql
@@ -0,0 +1,35 @@
+Given a ciphertext and adversary cannot distinguish if message is m0 or m1 for 
+
+The secrecy must reside entirely on the key
+
+00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+Gremio was the first team in Rio Grande do Sul to become Champion of South America, Inter was the second
+29141f11070b0c1409131e1e47130b59031f151b14141a0f09570b1a1d150b0411124d4c020c134611111d081714051b09074c
+
+
+Most powerful adversary learns nothing about the plaintext from the chipertext.
+Cryptography concerns the projection of trust: taking trust from where it exists a to an
+The secrecy must reside entirely on the key, not the ciph
+The design of a system should not require secrecy of the system.
+Cryptanalysis The art and science of analyzing weaknesses of cipher algorithms.
+Cryptography is typically bypassed, not penetrated.
+-K	VXWW
+M&T1
+FS>
+DC
+H
+
+
+4772656d696f2077617320746865206669727374207465616d20696e2052696f204772616e646520646f2053756c20746f206265636f6d65204368616d70696f6e206f6620536f75746820416d65726963612c20496e7465722077617320746865207365636f6e642e
+The secrecy must reside entirely on the keyAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+
+
+T h e   s e c r e c y   m u s t   r e s i d e   e n t i r e l y   o n   t h e   k e y ,   n o t   t h e   c i p h e r   a l g o r i . . m
+
+The secrecy must reside entirely on the key, not the cipher algorithm
+
+Given a ciphertext and adversary cannot distinguish if message is m0 or m1 for algorithmic
+
+algorithmic
+
+6a", "66", "66", "61", "73", "64", "6b", "66", "68", "63", "76", "67", "67", "7a", "78", "79", "77", "66", "65", "72", "77", "75", "76", "63", "70", "77", "69", "63", "6d", "74", "78", "77", "74", "76", "61", "6c", "6c", "63", "67", "66", "61", "74", "73", "6d", "63", "66", "64", "6f", "6c", "63", "62", "6e", "66", "65", "61", "74", "68", "6a", "64", "65", "63", "78", "67", "66", "66", "64", nil, nil, "6c", "6d", "62", "63", nil, nil, "64", nil, nil, nil, "39", nil, "62", nil, "27", "66", "21", "23", nil, nil, "36", "71", "39", "37"
